51d5f2b559c116ed79ded7672379d5362727b544
kurumihere <wdwdadwdadwadadw@gmail.com>
2025-10-03 20:13:46 +0500

    init commit
    Signed-off-by: kurumihere <wdwdadwdadwadadw@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+int main(int arg, char** av)
+{
+	std::cout << ">> ";
+	char in[100];
+	fgets(in, 100, stdin);
+	in[strlen(in) - 1] = '\0';
+	char* ctx = nullptr;
+	const char* a = strtok_s(in, " ", &ctx);
+	const char* b = strtok_s(nullptr, " ", &ctx);
+	const char* c = strtok_s(nullptr, " ", &ctx);
+	const double var = std::atof(a);
+	const double var0 = std::atof(c);
+	std::cout << a << " " << b << " " << c << " = ";
+ 	switch (*b) {
+		case 43:
+			std::cout << var + var0;
+			break;
+		case 45:
+			std::cout << var - var0;
+			break;
+		case 42:
+			std::cout << var * var0;
+			break;
+		case 47:
+			std::cout << var / var0;
+			break;
+		default: ;
+    }
+	return 0;
+}
