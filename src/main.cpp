321f3e728fdea8155e67bea3bb15064586580870
kurumihere <wdwdadwdadwadadw@gmail.com>
2025-10-06 18:40:31 +0500

    fix break

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
-
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
 #include <getopt.h>
-using namespace std;
 
 static struct option longopts[]=
 {
@@ -15,17 +13,17 @@ int
 main(int arg, char**av)
 {
 	int argc;
-	while ((argc=getopt_long(arg,av,shortopts,longopts, &argc))!=-1) {
+	while ((argc=getopt_long(arg,av,shortopts,longopts,&argc))!=-1) {
 		switch (argc) {
 			case 'h':
-				cout << "Type expression with tabs and press return, to see result." << endl;
+				std::cout << "Type expression with tabs and press return, to see result." << endl;
 			break;
 			default:
 			break;
 		}
 		return 1;
 	}
-	cout << ">> ";
+	std::cout << ">> ";
 	char in[100];
 	fgets(in, 100, stdin);
 	in[strlen(in) - 1] = '\0';
@@ -35,19 +33,19 @@ main(int arg, char**av)
 	const char*c = strtok(nullptr, " ");
 	const double var = strtod(a, &endptr);
 	const double var0 = strtod(c, &endptr);
-	cout << a << " " << b << " " << c << " = ";
+	std::cout << a << " " << b << " " << c << " = ";
  	switch (*b) {
 		case 43:
-			cout << var + var0;
+			std::cout << var + var0;
 			break;
 		case 45:
-			cout << var - var0;
+			std::cout << var - var0;
 	    	break;
 		case 42:
-			cout << var * var0;
+			std::cout << var * var0;
 			break;
 		case 47:
-			cout << var / var0;
+			std::cout << var / var0;
 			break;
 		default: ;
 	}
